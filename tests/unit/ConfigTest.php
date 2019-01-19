f2550a47ea9505228c74f6a638c4e42a48f96c9b
Dovydas Rupsys <dovydas.rupsys@cryptohaven.com>
2019-01-19 03:11:03 +0000

    Add Config tests

---
+++ tests/unit/ConfigTest.php
@@ -0,0 +1,10 @@
+<?php namespace MVF\Servicer\Tests;
+
+class ConfigTest extends \Codeception\Test\Unit
+{
+    public function testThatSkipReturnsFalse()
+    {
+        $config = $this->make(\MVF\Servicer\Config::class);
+        self::assertEquals(false, $config->skip());
+    }
+}
