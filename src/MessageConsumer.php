ebced29bfa465f5061e0ad06fc15fd09620dae88
Ellie Wintram <eleanor.wintram@mvfglobal.com>
2019-06-10 14:19:58 +0100

    MER-1054: Uncomment standard logging in MessageConsumer

--- src/MessageConsumer.php
+++ src/MessageConsumer.php
@@ -25,16 +25,16 @@ class MessageConsumer
     public static function consume(ActionInterface $action, array $headers, array $body): callable
     {
         return function () use ($action, $headers, $body) {
-//            $reflect = new ReflectionClass($action);
+            $reflect = new ReflectionClass($action);
 //
 //            if (isset($headers['carrier']) === true) {
 //                GlobalTracer::get()->extract('text_map', $headers['carrier']);
 //            }
 //
 //            $span = GlobalTracer::get()->startActiveSpan($reflect->getShortName())->getSpan();
-//            self::log('INFO', $reflect->getShortName(), 'STARTED', $headers, $body);
+            self::log('INFO', $reflect->getShortName(), 'STARTED', $headers, $body);
             $action->handle($headers, $body);
-//            self::log('INFO', $reflect->getShortName(), 'COMPLETED', $headers, $body);
+            self::log('INFO', $reflect->getShortName(), 'COMPLETED', $headers, $body);
 //            $span->finish();
         };
     }
